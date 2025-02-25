8036733d0fc3ec4e057062cd9baa8356c5a4dca7
Thomas D'HULST <tdhulst@ippon.fr>
2025-02-25 16:17:11 +0100

    feat(vision): et la meilleur façon de read

--- lib/views/city_finder_screen.dart
+++ lib/views/city_finder_screen.dart
@@ -1,6 +1,9 @@
 import 'package:atelier1/logic/city_provider.dart';
 import 'package:atelier1/models/place.dart';
 import 'package:atelier1/views/place_detail_screen.dart';
+import 'package:atelier1/views/widgets/dio_error_widget.dart';
+import 'package:atelier1/views/widgets/found_city_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,34 +15,7 @@ class CityFinderScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textController = useTextEditingController();
 
-    // ref.listen(cityNotifierProvider, (prev, next) {
-    //   if (prev?.isLoading ?? false) {
-    //     if (next.hasError) {
-    //       showErrorDialog(context);
-    //     } else if (next.hasValue) {
-    //       navigateToDetail(context, next.value!.places.first);
-    //     }
-    //   }
-    // });
-
-    ref
-      ..listen(cityNotifierProvider.select((state) => state.error), (
-        prev,
-        next,
-      ) {
-        if (next != null) {
-          showErrorDialog(context);
-        }
-      })
-      ..listen(cityNotifierProvider.select((state) => state.value), (
-        prev,
-        next,
-      ) {
-        if (next != null) {
-          navigateToDetail(context, next.places.first);
-        }
-      });
-
+    final cityFound = ref.watch(cityNotifierProvider);
     return Scaffold(
       appBar: AppBar(title: Text('City Finder')),
       resizeToAvoidBottomInset: false,
@@ -71,6 +47,19 @@ class CityFinderScreen extends HookConsumerWidget {
               ),
             ),
             SizedBox(height: 16),
+            cityFound.when(
+              data:
+                  (data) =>
+                      data != null
+                          ? FoundCityCard(city: data)
+                          : Text('not found'),
+              error:
+                  (err, st) =>
+                      err is DioException
+                          ? DioErrorWidget(error: err)
+                          : Text(err.toString()),
+              loading: () => CircularProgressIndicator(),
+            ),
           ],
         ),
       ),
